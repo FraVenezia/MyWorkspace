b6224aaae15b9411cdfa1e45c4b287a464486124
FraVenezia <fra.venezia@gmail.com>
2018-06-22 21:58:18 +0200

    Aggiunta GestRifugio

---
+++ GestRifugio/src/main/java/it/rifugio/model/Customer.java
@@ -0,0 +1,5 @@
+package it.rifugio.model;
+
+public class Customer {
+
+}
---
+++ GestRifugio/src/main/java/it/rifugio/model/Room.java
@@ -0,0 +1,9 @@
+package it.rifugio.model;
+
+public class Room {
+	private int idRoom;
+	private int num;
+	private String typeRoom;
+	private int beds;
+
+}
